3ab81e46b308e9f90767eecc84ceeef8763fa8e0
MefDev <mohamedmeftah2018@gmail.com>
2023-11-24 13:55:19 -0800

    create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp
-file {'/tmp/school':
-  ensure  => 'file',
+file {'school':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
+  content => 'I love Puppet',
 }
