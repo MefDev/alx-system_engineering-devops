9d7cebdb41c00ef60539ec5c6761cb5282ec3c6e
mefdev <mohamedmeftah2018@gmail.com>
2023-12-06 00:45:20 +0100

    configure two servers using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,15 @@
 # Using Puppet, configure two servers 
-exec {'apt-update':
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
 -> package { 'nginx':
   ensure   => installed,
 }
--> file_line { 'add-header':
+-> file_line { 'add HTTP header':
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _',
-  line   => "add_header X-Served-By ${hostname}",
+  after  => 'server_name _;',
+  line   => "\n\tadd_header X-Served-By \"${hostname}\";",
 }
--> exec { 'restart-service':
-  command  => '/usr/sbin/service nginx restart',
+-> exec { 'restart nginx':
+    command  => '/usr/sbin/service nginx restart',
 }
