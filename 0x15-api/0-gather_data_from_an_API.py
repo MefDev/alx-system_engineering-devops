2de49114543ee25e89c6ec0d5a48e709bee6bbb9
mefdev <mohamedmeftah2018@gmail.com>
2024-01-03 00:58:58 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+"""Python script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress."""
 if __name__ == "__main__":
     import requests
     import sys
