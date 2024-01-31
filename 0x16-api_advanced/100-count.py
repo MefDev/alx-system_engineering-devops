4b08905e45dc1798280b997e915a74256087b763
Mefdev <mefdev.pro@gmail.com>
2024-01-31 23:37:07 +0100

    Parse the title of all hot articles

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given keywords ("""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False).json()
+    if response.get("error") == 404:
+        return None
+    for obj in response.get("data").get("children"):
+        word_list.append(obj.get("data").get("title"))
+        after = response.get("data").get("after")
+        if after:
+            url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+                subreddit, after)
+            headers = {"User-Agent": "My-User-Agent"}
+            response = requests.get(url, headers=headers,
+                                    allow_redirects=False).json()
+            for obj in response.get("data").get("children"):
+                word_list.append(obj.get("data").get("title"))
+            after = response.get("data").get("after")
+            if after:
+                count_words(subreddit, word_list)
