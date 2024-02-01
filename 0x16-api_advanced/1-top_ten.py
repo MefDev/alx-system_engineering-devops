6d23809f5361e2c5e0cc6f728e91f747d2baa77e
Mefdev <mefdev.pro@gmail.com>
2024-02-01 20:44:29 +0100

    Parse the title of all hot articles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Module 1"""
+"""Prints the titles of the first 10 hot posts listed for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
     """Returns the number of subscribers for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=2".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     response = requests.get(url, headers=headers, allow_redirects=False).json()
     if response.get("error") == 404:
         print("None")
         return
-    for obj in response.get("data").get("children"):
-        print(obj.get("data").get("title"))
+    else:
+        for obj in response.get("data").get("children"):
+            print(obj.get("data").get("title"))
