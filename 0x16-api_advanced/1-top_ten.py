e9f04f07d697ea64e4337d33a12c1e38c6edf7ee
Mefdev <mefdev.pro@gmail.com>
2024-02-01 20:50:11 +0100

    print the number of subs for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,18 @@ import requests
 
 
 def top_ten(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=2".format(subreddit)
+    """prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    if subreddit is None:
+        return 0
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers, allow_redirects=False).json()
-    if response.get("error") == 404:
-        print("None")
-        return
+    params = {"limit": 10}
+    response = requests.get(url, params=params, headers=headers,
+                            allow_redirects=False).json()
+    data = response.get("data", {}).get("children", None)
+    if data:
+        for topic in data:
+            print(topic.get("data").get("title"))
     else:
-        for obj in response.get("data").get("children"):
-            print(obj.get("data").get("title"))
+        print("None")
