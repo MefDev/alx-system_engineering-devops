32d6b57ab2880b9de2649fac9f69aa08101af027
Mefdev <mefdev.pro@gmail.com>
2024-02-01 20:29:34 +0100

    print the number of subs for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Module for task 0"""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
-    import requests
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False).json()
-    if response.status_code >= 300:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        return response.json().get('data', {}).get('subscribers', 0)
+    except requests.exceptions.RequestException as e:
         return 0
-    return response.json().get("data").get("subscribers")
