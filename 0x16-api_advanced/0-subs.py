31b2007586fcf70641ca28385307b20dfa77e102
Mefdev <mefdev.pro@gmail.com>
2024-03-01 22:59:13 +0100

    Add subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Get the number of subscribers for a particular subreddit"""
+"""Get the number of subscribers for a subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
+    """Get the number of subscribers for a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers)
         data = response.json()
         return data.get('data', {}).get('subscribers', 0)
     except requests.exceptions.RequestException as e:
